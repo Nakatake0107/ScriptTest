c0cb10aeed09367a2e3ce406460449823cbe2c67
Nakatake0107 <85175549+Nakatake0107@users.noreply.github.com>
2021-06-29 21:21:15 +0900

    修正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {// Use this for initialization
     void Start()
-{
-    // 要素数5の配列を初期化する
-    int[] array = new int[5];
-
-    // 配列の各要素に値を代入する
-    array[0] = 30;
-    array[1] = 20;
-    array[2] = 50;
-    array[3] = 10;
-    array[4] = 80;
-
-    //5回処理を繰り返す
-    for (int i = 0; i < 5; i++)
     {
-        Debug.Log(array[i]);
-    }
-    //5回処理を逆順に繰り返す
-    for (int i = 0; i >= 5; i--)
-    {
-        Debug.Log(array[i]);
-    }
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
 
+        // 配列の各要素に値を代入する
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+
+        //5回処理を繰り返す
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        //5回処理を逆順に繰り返す
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
